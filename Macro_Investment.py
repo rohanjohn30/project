618fbbb43e3981d4ff024bf4741ceb23ea346850
Rohan John <rohanjohn.1990@gmail.com>
2017-03-15 15:30:42 +0100

    Added Main docstring and imported modules required.

--- Macro_Investment.py
+++ Macro_Investment.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 15 15:23:44 2017
-
-@author: Rohan John
+This program runs a multivariate regression for Keynesian Investment function.
+The independent variables are business sentiment, GDP, funds Rate and R&D.
+The dependent varibale is Investment rate.
+The data is automatically downloaded from the Federal Reserve.
 """
 
+from pandas import read_csv
+import pandas.io.data as web
+import matplotlib.pyplot as plt
+import datetime as dt
+import os
+import subprocess
+from statsmodels.formula.api import ols
+from statsmodels.iolib.summary2 import summary_col
+import statsmodels.api as sm
+
+start = dt.datetime(1970, 1, 1)     #sets the start year of analysis. 1970 seems to be the starting year for most data sets.
+end = dt.datetime(2017, 1, 1)      #sets the end year of analysis. 
