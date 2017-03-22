63cbfe0d20f06d9afff5b2ab220c030a760d5e60
Rohan John <rohanjohn.1990@gmail.com>
2017-03-22 13:36:32 +0100

    added the codes for downloading and sorting data

--- Macro_Investment.py
+++ Macro_Investment.py
@@ -18,3 +18,22 @@ import statsmodels.api as sm
 
 start = dt.datetime(1970, 1, 1)     #sets the start year of analysis. 1970 seems to be the starting year for most data sets.
 end = dt.datetime(2017, 1, 1)      #sets the end year of analysis. 
+
+ con = web.DataReader(["A191RL1Q225SBEA","BSCICP02USQ460S",
+                          "A006RL1A225NBEA","IRSTFR01USA156N","Y006RL1A225NBEA"], 
+                            "fred", start, end)
+    con.ix['2013-01-01']
+    con.to_csv('raw.csv')
+    con = read_csv('raw.csv')
+    con.columns = ['Date', 'GDP','bsent', 'investment','rate','research']
+    #con['lagGDP'] = con['GDP'].shift(-1)
+    con.to_csv('test.csv')
+    os.remove('raw.csv')
+    f= read_csv("test.csv")
+    keep_col = ['Date', 'GDP','bsent','investment','rate','research']
+    new_f = f[keep_col]
+    new_f.dropna(how='any', inplace=True)
+    new_f.to_csv('data.csv', index=False) # creates csv file with the given headers
+    new_f.head()
+    os.remove('test.csv')
+    return new_f
